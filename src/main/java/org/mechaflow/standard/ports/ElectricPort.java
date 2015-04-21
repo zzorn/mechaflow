44525917216e7169893e3406e2039c4ad8ca9da1
zzorn <zzorn@iki.fi>
2015-04-21 03:37:15 +0300

    Should drop the electricity simulation and do power simulation instead. Added some comments to that effect.

--- src/main/java/org/mechaflow/standard/ports/ElectricPort.java
+++ src/main/java/org/mechaflow/standard/ports/ElectricPort.java
@@ -7,6 +7,16 @@ import org.mechaflow.PortDirection;
 /**
  * Port for transmission of electric power
  */
+// TODO: Drop Electricity simulation, instead just add support for power transmission (through electricity or rotation, specify the
+// TODO  transfer voltages and the available power (or transfer RPM and available power).
+// TODO  Certain applications may require a certain voltage or RPM level, in which case other components may be used to transform the
+// TODO  voltage or RPM level.  Each component using (or providing) some power removes (or adds) it to the power bus.
+// TODO  Excess energy may be distributed with some weights?
+// TODO  Producers could record how much they offer to produce, and then not produce the part that was not needed
+// TODO  In some cases the power is produced in any case, and needs to be sunk somewhere?
+// NOTE: Signals can be used instead of electricity to transport information and do computation that electricity would
+// NOTE  normally have been used for
+// NOTE: If we use power ports, we only need to update things when a state changes, speeding up processing considerably
 public final class ElectricPort extends PortBase {
 
     // TODO: Information on maximum supported current and voltage, and handling of failure conditions
